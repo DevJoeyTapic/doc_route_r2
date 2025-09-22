b7daf0f57ac2803983d73be82e937f5e873980ba
Joey Tapic <joeyt@wallem.com.ph>
2025-09-22 10:44:20 +0800

    static validation working, swithing to api auth

--- frontend/app/src/PinPage.tsx
+++ frontend/app/src/PinPage.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
+import axios from "axios";
 
 function PinPage() {
   const [pin, setPin] = useState<string[]>(["", "", "", ""]);
