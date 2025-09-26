f76758b6bd59e7f8645bbeec51de6c5e1cec6c5e
Joey Tapic <joeyt@wallem.com.ph>
2025-09-26 17:23:54 +0800

    migrated to  db container

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -71,10 +71,21 @@ WSGI_APPLICATION = 'server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'doc_route_db',
+        'USER': 'root',
+        'PASSWORD': 'rootpassword',
+        'HOST': 'db',  # or server IP in production
+        'PORT': '3306',
     }
 }
 
