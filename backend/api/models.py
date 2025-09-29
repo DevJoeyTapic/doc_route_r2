e40dca83991cbcb9857f5fc38867a6b49d20823a
Joey Tapic <joeyt@wallem.com.ph>
2025-09-29 17:38:35 +0800

    Model created for Invoice and Supplier

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,4 @@
+import uuid
 from django.db import models
 from django.contrib.auth.hashers import make_password,check_password
 
@@ -18,4 +19,36 @@ class Pin(models.Model):
     
     def __str__(self):
         return f"PIN for {self.supplier_id}"
-    
+
+class Invoice(models.Model):
+    sumitted_date = models.DateTimeField
+    invoice_number = models.CharField(max_length=100, unique=True)
+    supplier = models.CharField(max_length=150)
+    amount_due = models.DecimalField(max_digits=12,decimal_places=2)
+    description = models.TextField(blank=True,null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-date_created"]
+        verbose_name = "Invoice" 
+
+    def __str__(self):
+        return f"Invoice {self.invoice_number} from {self.supplier}"  
+
+class Supplier(models.Model):
+    supplier_id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+    supplier_name = models.CharField(max_length=150)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-date_created']
+        verbose_name = "Supplier"
+        verbose_name_plural = "Suppliers"
+
+    def __str__(self):
+        return f"{self.supplier_name} ({self.supplier_id})"
